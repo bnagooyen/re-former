9ff911066f90446acdbfe7f48b99e059013756ca
Brian Nguyen <briannguyen33@gmail.com>
2016-06-06 17:22:35 -0700

    styling, css, bootstrap add

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
+  root to: 'static_pages#home'
+
+  get 'static_pages/about'
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+	resources :users
 end
